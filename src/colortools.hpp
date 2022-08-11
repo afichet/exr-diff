7266241bda24fe8f2170ae514d30749618473050
Alban Fichet <alban.fichet@gmx.fr>
2022-08-11 18:09:13 +0200

    Fixing errors in the Delta E 2000 computation

--- src/colortools.hpp
+++ src/colortools.hpp
@@ -100,95 +100,105 @@ Float deltaE2000(const Float Lab_1[3], const Float Lab_2[3])
     const Float a2 = Lab_2[1];
     const Float b2 = Lab_2[2];
 
-    const Float bar_L_prime = (L1 + L2) / 2.;
+    const Float C_star_1 = std::sqrt(a1 * a1 + b1 * b1);
+    const Float C_star_2 = std::sqrt(a2 * a2 + b2 * b2);
+    const Float bar_C_star = (C_star_1 + C_star_2) / 2.;
 
-    const Float C1 = std::sqrt(a1 * a1 + b1 * b1);
-    const Float C2 = std::sqrt(a2 * a2 + b2 * b2);
+    Float bar_C_start_p7 = bar_C_star * bar_C_star;   // ^2
+    bar_C_start_p7 = bar_C_start_p7 * bar_C_start_p7 * bar_C_start_p7 * bar_C_star; // ^7
 
-    const Float bar_C = (C1 + C2) / 2.;
-
-    // bar_C**7
-    Float bar_C_pow7 = bar_C * bar_C;                            // bar_C**2
-    bar_C_pow7 = bar_C_pow7 * bar_C_pow7 * bar_C_pow7 * bar_C;   // bar_C**7
-
-    const Float G
-        = .5 * (1 - std::sqrt(bar_C_pow7 / (bar_C_pow7 + 6103515625)));
-
-    const Float a_prime_1 = a1 * (1 + G);
-    const Float a_prime_2 = a2 * (1 + G);
-
+    const Float G = .5 * (1. - std::sqrt((bar_C_start_p7 / (bar_C_start_p7 + 6103515625.))));
+    const Float a_prime_1 = (1. + G) * a1;
+    const Float a_prime_2 = (1. + G) * a2;
+    // std::cout << ">> " << bar_C_start_p7 / (bar_C_start_p7 + 6103515625.) << std::endl;
     const Float C_prime_1 = std::sqrt(a_prime_1 * a_prime_1 + b1 * b1);
     const Float C_prime_2 = std::sqrt(a_prime_2 * a_prime_2 + b2 * b2);
 
+    Float h_prime_1_rad = 0;
+
+    if (b1 != 0 || a_prime_1 != 0) {
+        h_prime_1_rad = std::atan2(b1, a_prime_1);
+
+        if (h_prime_1_rad < 0) {
+            h_prime_1_rad += 2. * M_PI;
+        }
+    }
+
+    Float h_prime_2_rad = 0;
+
+    if (b2 != 0 || a_prime_2 != 0) {
+        h_prime_2_rad = std::atan2(b2, a_prime_2);
+
+        if (h_prime_2_rad < 0) {
+            h_prime_2_rad += 2. * M_PI;
+        }
+    }
+
+    const Float p_C_prime_12 = C_prime_1 + C_prime_2;
+
+    Float Delta_h_prime_rad = 0;
+
+    if (p_C_prime_12 != 0) {
+        if (std::abs(h_prime_2_rad - h_prime_1_rad) <= M_PI) {
+            Delta_h_prime_rad = h_prime_2_rad - h_prime_1_rad;
+        } else if (h_prime_2_rad - h_prime_1_rad > M_PI) {
+            Delta_h_prime_rad = h_prime_2_rad - h_prime_1_rad - 2. * M_PI;
+        } else {
+            Delta_h_prime_rad = h_prime_2_rad - h_prime_1_rad + 2. * M_PI;
+        }
+    }
+
+    const Float Delta_L_prime = L2 - L1;
+    const Float Delta_C_prime = C_prime_2 - C_prime_1;
+    const Float Delta_H_prime = 2. * std::sqrt(C_prime_1 * C_prime_2) * std::sin(Delta_h_prime_rad / 2.);
+
+    const Float bar_L_prime = (L1 + L2) / 2.;
     const Float bar_C_prime = (C_prime_1 + C_prime_2) / 2.;
 
-    Float h_prime_1 = std::atan2(b1, a_prime_1);
-    if (h_prime_1 < 0) {
-        h_prime_1 += 2. * M_PI;
-    }
+    Float bar_h_prime_rad = 0;
 
-    Float h_prime_2 = std::atan2(b2, a_prime_2);
-    if (h_prime_2 < 0) {
-        h_prime_2 += 2. * M_PI;
-    }
-
-    const Float diff_h_prime = std::abs(h_prime_1 - h_prime_2);
-
-    Float bar_H_prime;
-    if (diff_h_prime > M_PI) {
-        bar_H_prime = (h_prime_1 + h_prime_2 + 2. * M_PI) / 2.;
+    if (p_C_prime_12 == 0) {
+        bar_h_prime_rad = h_prime_1_rad + h_prime_2_rad;
+    } else if (std::abs(h_prime_1_rad - h_prime_2_rad) <= M_PI) {
+        bar_h_prime_rad = (h_prime_1_rad + h_prime_2_rad) / 2.;
+    } else if (h_prime_1_rad + h_prime_2_rad < 2. * M_PI) {
+        bar_h_prime_rad = (h_prime_1_rad + h_prime_2_rad + 2. * M_PI) / 2.;
     } else {
-        bar_H_prime = (h_prime_1 + h_prime_2) / 2.;
+        bar_h_prime_rad = (h_prime_1_rad + h_prime_2_rad - 2. * M_PI) / 2.;
     }
 
-    const Float T = 1. - 0.17 * std::cos(bar_H_prime - M_PI / 6.)
-                    + 0.24 * std::cos(2. * bar_H_prime)
-                    + 0.32 * std::cos(3. * bar_H_prime + M_PI / 30.)
-                    - 0.20 * std::cos(4. * bar_H_prime - 7 * M_PI / 20.);
+    const Float T = 1.
+        - 0.17 * std::cos(     bar_h_prime_rad - M_PI / 6.)
+        + 0.24 * std::cos(2. * bar_h_prime_rad)
+        + 0.32 * std::cos(3. * bar_h_prime_rad + M_PI / 30.)
+        - 0.20 * std::cos(4. * bar_h_prime_rad - 7. * M_PI / 20.);
 
-    Float delta_h_prime;
-    if (diff_h_prime <= M_PI) {
-        delta_h_prime = h_prime_2 - h_prime_1;
-    } else if (diff_h_prime > M_PI && h_prime_2 <= h_prime_1) {
-        delta_h_prime = h_prime_2 - h_prime_1 + 2. * M_PI;
-    } else {
-        delta_h_prime = h_prime_2 - h_prime_1 - 2. * M_PI;
-    }
+    const Float exp_v = (bar_h_prime_rad * 180. / M_PI - 275.) / 25.;
+    const Float Delta_theta_rad = M_PI / 6. * std::exp(-exp_v * exp_v);
 
-    const Float delta_L_prime = L2 - L1;
-    const Float delta_C_prime = C_prime_2 - C_prime_1;
-    const Float delta_H_prime
-        = 2. * std::sqrt(C_prime_1 * C_prime_2) * std::sin(delta_h_prime / 2.);
+    Float bar_C_prime_p7 = bar_C_prime * bar_C_prime;
+    bar_C_prime_p7 = bar_C_prime_p7 * bar_C_prime_p7 * bar_C_prime_p7 * bar_C_prime;
 
-    const Float bar_L_prime_minus50_sqr
-        = (bar_L_prime - 50.) * (bar_L_prime - 50.);
-    const Float S_L = 1.
-                      + (0.015 * bar_L_prime_minus50_sqr)
-                            / std::sqrt(20. + bar_L_prime_minus50_sqr);
+    Float bar_L_prime_m50_p2 = bar_L_prime - 50.;
+    bar_L_prime_m50_p2 =bar_L_prime_m50_p2 * bar_L_prime_m50_p2;
+
+    const Float R_C = 2. * std::sqrt(bar_C_prime_p7 / (bar_C_prime_p7 + 6103515625.));
+    const Float S_L = 1. + 0.015 * bar_L_prime_m50_p2 / std::sqrt(20. + bar_L_prime_m50_p2);
     const Float S_C = 1. + 0.045 * bar_C_prime;
     const Float S_H = 1. + 0.015 * bar_C_prime * T;
-
-    const Float bar_H_prime_r = ((bar_H_prime * 180. / M_PI) - 275.) / 25.;
-    const Float delta_theta   = 30. * std::exp(-bar_H_prime_r * bar_H_prime_r);
-
-
-    Float bar_C_prime_pow7 = bar_C_prime * bar_C_prime;   // bar_C**2
-    bar_C_prime_pow7 = bar_C_prime_pow7 * bar_C_prime_pow7 * bar_C_prime_pow7
-                       * bar_C_prime;   // bar_C**7
-
-    const Float R_C
-        = 2. * std::sqrt(bar_C_prime_pow7 / (bar_C_prime_pow7 + 6103515625));
-    const Float R_T = -R_C * std::sin(2. * delta_theta);
+    const Float R_T = -std::sin(2. * Delta_theta_rad) * R_C;
 
     const Float K_L = 1.;
     const Float K_C = 1.;
     const Float K_H = 1.;
 
-    const Float delta_L_r = delta_L_prime / (K_L * S_L);
-    const Float delta_C_r = delta_C_prime / (K_C * S_C);
-    const Float delta_H_r = delta_H_prime / (K_H * S_H);
+    const Float delta_L_r = Delta_L_prime / (K_L * S_L);
+    const Float delta_C_r = Delta_C_prime / (K_C * S_C);
+    const Float delta_H_r = Delta_H_prime / (K_H * S_H);
 
     return std::sqrt(
-        delta_L_r * delta_L_r + delta_C_r * delta_C_r + delta_H_r * delta_H_r
+          delta_L_r * delta_L_r
+        + delta_C_r * delta_C_r
+        + delta_H_r * delta_H_r
         + R_T * delta_C_r * delta_H_r);
 }
